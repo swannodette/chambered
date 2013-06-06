22ec74988bdab6af5234a01a992762161e9978dd
David Nolen <david.nolen@gmail.com>
2013-06-06 00:24:36 -0400

    boom we have the textures

--- src/chambered/core.cljs
+++ src/chambered/core.cljs
@@ -54,24 +54,9 @@
 
 ;; for figuring out what Notch was thinking
 
-(defn red [n]
-  (bit-and (bit-shift-right n 16) 0xFF))
-
-(defn green [n]
-  (bit-and (bit-shift-right n 8) 0xFF))
-
-(defn blue [n]
-  (bit-and n 0xFF))
-
-#_(defn copy-texmap-into-pixels [texmap pixels]
-  (forloop [(i 0) (< i (alength texmap)) (inc i)]
-    (let [n  (aget texmap i)
-          pi (* i 4)
-          data (.-data pixels)]
-      (aset data (+ pi 0) (red n))
-      (aset data (+ pi 1) (green n))
-      (aset data (+ pi 2) (blue n))
-      (aset data (+ pi 3) (alpha n)))))
+(defn red [n] (bit-and (bit-shift-right n 16) 0xFF))
+(defn green [n] (bit-and (bit-shift-right n 8) 0xFF))
+(defn blue [n] (bit-and n 0xFF))
 
 (defn set-pixel [pixels x y c]
   (let [width  (.-width pixels)
@@ -81,7 +66,7 @@
     (aset data (+ pi 0) (red c))
     (aset data (+ pi 1) (green c))
     (aset data (+ pi 2) (blue c))
-    (aset data (+ pi 3) 255)))
+    (aset data (+ pi 3) 0xFF)))
 
 (defn copy-texmap-into-pixels [texmap pixels]
   (let [counter (Box. 0)]
@@ -92,17 +77,7 @@
             (set-pixel pixels x (+ y (* i 16 3)) c)
             (reset! counter (inc @counter))))))))
 
-(defn non-zero [arr]
-  (loop [i 0]
-    (if (< i (alength arr))
-      (let [n (aget texmap i)]
-        (if (and n (not (zero? n)))
-          (array i n)
-          (recur (inc i)))))))
-
 (declare clock)
-
-(def counter 0)
 
 ;; Notch's clever texture generator, we use Boxes since we can't bash
 ;; on locals, data looks completely different! - David
@@ -121,12 +96,8 @@
             (reset! br (- 255 (random-int 96))))
           (when (== i 1)
             (cond
-              (< y (+ (bitop x) 18))
-              (reset! color 0x6AAA40)
-
-              (< y (+ (bitop x) 19))
-              (do
-                (reset! br (/ (* @br 2) 3)))))
+              (< y (+ (bitop x) 18)) (reset! color 0x6AAA40)
+              (< y (+ (bitop x) 19)) (reset! br (/ (* @br 2) 3))))
           ;; (when (== i 7)
           ;;   (reset! color 0x675231)
           ;;   (when (and (in? x 0 15) (or (in? y 0 15) (in? y 32 47)))
@@ -160,10 +131,9 @@
           (let [c   @color
                 brr @brr
                 rc (bit-or
-                     (bit-or (color-int c brr 16)
-                       (color-int c brr 8))
-                     (color-int brr c))]
-            (.log js/console y c brr rc)
+                     (color-int c brr 16)
+                     (color-int c brr 8)
+                     (color-int c brr))]
             (aset texmap (+ x (* y 16) (* i 256 3)) rc)
             ))))
 
