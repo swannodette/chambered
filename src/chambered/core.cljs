0e0460527b694da66661bbe60deb6b71c6de3317
David Nolen <david.nolen@gmail.com>
2013-06-06 20:52:40 -0400

    move testing stuff in util file

--- src/chambered/core.cljs
+++ src/chambered/core.cljs
@@ -57,34 +57,6 @@
       (bit-and 0xFF) (* brr) (/ 255)
       (bit-shift-left shift))))
 
-;; for figuring out what Notch was thinking
-
-(defn red [n] (bit-and (bit-shift-right n 16) 0xFF))
-(defn green [n] (bit-and (bit-shift-right n 8) 0xFF))
-(defn blue [n] (bit-and n 0xFF))
-
-(defn set-pixel [pixels x y c]
-  (let [width  (.-width pixels)
-        height (.-height pixels)
-        data   (.-data pixels)
-        pi     (* (+ x (* y width)) 4)]
-    (aset data (+ pi 0) (red c))
-    (aset data (+ pi 1) (green c))
-    (aset data (+ pi 2) (blue c))
-    (aset data (+ pi 3) 0xFF)))
-
-;; for debugging textures
-(defn copy-texmap-into-pixels [texmap pixels]
-  (let [counter (Box. 0)]
-    (forloop [(i 0) (< i 16) (inc i)]
-      (forloop [(y 0) (< y (* 16 3)) (inc y)]
-        (forloop [(x 0) (< x 16) (inc x)]
-          (let [c (aget texmap @counter)]
-            (set-pixel pixels
-              (+ x (* (.floor js/Math (/ i 5)) 16))
-              (+ y (mod (* i 16 3) (* 16 3 5))) c)
-            (reset! counter (inc @counter))))))))
-
 (declare clock)
 
 ;; Notch's clever texture generator, we use Boxes since we can't bash
---
+++ src/chambered/util.cljs
@@ -0,0 +1,29 @@
+(ns chambered.util)
+
+;; for figuring out what Notch was thinking
+
+(defn red [n] (bit-and (bit-shift-right n 16) 0xFF))
+(defn green [n] (bit-and (bit-shift-right n 8) 0xFF))
+(defn blue [n] (bit-and n 0xFF))
+
+(defn set-pixel [pixels x y c]
+  (let [width  (.-width pixels)
+        height (.-height pixels)
+        data   (.-data pixels)
+        pi     (* (+ x (* y width)) 4)]
+    (aset data (+ pi 0) (red c))
+    (aset data (+ pi 1) (green c))
+    (aset data (+ pi 2) (blue c))
+    (aset data (+ pi 3) 0xFF)))
+
+;; for debugging textures
+(defn copy-texmap-into-pixels [texmap pixels]
+  (let [counter (Box. 0)]
+    (forloop [(i 0) (< i 16) (inc i)]
+      (forloop [(y 0) (< y (* 16 3)) (inc y)]
+        (forloop [(x 0) (< x 16) (inc x)]
+          (let [c (aget texmap @counter)]
+            (set-pixel pixels
+              (+ x (* (.floor js/Math (/ i 5)) 16))
+              (+ y (mod (* i 16 3) (* 16 3 5))) c)
+            (reset! counter (inc @counter))))))))
